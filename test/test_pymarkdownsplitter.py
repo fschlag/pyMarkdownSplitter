ad61e3e3e0c0473191c9de6afa5bc79612ea42d2
fschlag <github@florianschlag.de>
2018-12-26 18:48:35 +0100

    Tests

--- test/test_pymarkdownsplitter.py
+++ test/test_pymarkdownsplitter.py
@@ -6,6 +6,11 @@ import shutil
 TEST_RESOURCES = "test/test_resources/"
 TMP_WORK_DIR = "tmp_outdir/"
 
+MANUAL_FILE = TMP_WORK_DIR + "manual.md"
+INSERT_AND_UPDATE_FILE = TMP_WORK_DIR + "insert-and-update.md"
+FIND_AND_QUERY_FILE = TMP_WORK_DIR + "find-and-query.md"
+ENTITY_ADAPTER_AND_DESCRIPTOR_FILE = TMP_WORK_DIR + "entity-adapter-and-descriptor.md"
+
 
 class PyMarkdownSplitterTest(unittest.TestCase):
 
@@ -13,32 +18,36 @@ class PyMarkdownSplitterTest(unittest.TestCase):
         if os.path.isdir(TMP_WORK_DIR):
             shutil.rmtree(TMP_WORK_DIR)
 
-    def assertFileExistsAndContainsTest(self, filename, text):
-        self.assertTrue(os.path.isfile(filename))
+    def assertFileContainsText(self, filename, text):
         with open(filename, 'r') as f:
             self.assertTrue(text in f.read())
 
+    def assertFileExistsAndContainsText(self, filename, text):
+        self.assertTrue(os.path.isfile(filename))
+        self.assertFileContainsText(filename, text)
+
     def testComprehensiveFile(self):
-        """Test a comprensive file; creates two sub-section files, creates index file; converts local links to global links"""
+        """Test a comprensive file"""
         pymarkdownsplitter.work(TEST_RESOURCES + "manual_comprehensive.md", TMP_WORK_DIR)
 
-        MANUAL_FILE = TMP_WORK_DIR + "manual.md"
-        INSERT_AND_UPDATE_FILE = TMP_WORK_DIR + "insert-and-update.md"
-        FIND_AND_QUERY_FILE = TMP_WORK_DIR + "find-and-query.md"
-        ENTITY_ADAPTER_AND_DESCRIPTOR_FILE = TMP_WORK_DIR + "entity-adapter-and-descriptor.md"
-
         # Test Index File
-        self.assertFileExistsAndContainsTest(MANUAL_FILE, "[Find and Query](find-and-query.html)")
-        self.assertFileExistsAndContainsTest(MANUAL_FILE, "[Insert and Update](insert-and-update.html)")
-        self.assertFileExistsAndContainsTest(MANUAL_FILE, "[Entity, Adapter, and Descriptor](entity-adapter-and-descriptor.html)")
+        self.assertFileExistsAndContainsText(MANUAL_FILE, "[Find and Query](find-and-query.html)")
+        self.assertFileExistsAndContainsText(MANUAL_FILE, "[Insert and Update](insert-and-update.html)")
+        self.assertFileExistsAndContainsText(MANUAL_FILE, "[Entity, Adapter, and Descriptor](entity-adapter-and-descriptor.html)")
         # Test correct heading for sub-section files
-        self.assertFileExistsAndContainsTest(INSERT_AND_UPDATE_FILE, "% Insert and Update")
+        self.assertFileExistsAndContainsText(INSERT_AND_UPDATE_FILE, "% Insert and Update")
         # Test local link to global link conversion
-        self.assertFileExistsAndContainsTest(FIND_AND_QUERY_FILE, "[local link](insert-and-update.html)")
-        self.assertFileExistsAndContainsTest(FIND_AND_QUERY_FILE, "[local link](entity-adapter-and-descriptor.html)")
+        self.assertFileExistsAndContainsText(FIND_AND_QUERY_FILE, "[local link](insert-and-update.html)")
+        self.assertFileExistsAndContainsText(FIND_AND_QUERY_FILE, "[local link](entity-adapter-and-descriptor.html)")
 
-        self.assertTrue(os.path.isfile(ENTITY_ADAPTER_AND_DESCRIPTOR_FILE))
+        # Test for correct section links
+        self.assertFileContainsText(FIND_AND_QUERY_FILE, "[&uarr; Index](manual.html)")
+        self.assertFileContainsText(FIND_AND_QUERY_FILE, "[Next Section &rarr;](insert-and-update.html)")
 
+        self.assertFileContainsText(INSERT_AND_UPDATE_FILE, "[&larr; Previous Section](find-and-query.html)")
+        self.assertFileContainsText(INSERT_AND_UPDATE_FILE, "[Next Section &rarr;](entity-adapter-and-descriptor.html)")
+
+        self.assertFileExistsAndContainsText(ENTITY_ADAPTER_AND_DESCRIPTOR_FILE, "[&larr; Previous Section](insert-and-update.html)")
 
 
 if __name__ == '__main__':
